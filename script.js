6ce3e3a3c858eddfc86b3bdfef5bd8d08d2133e2
amfv-obeyed <alexverral@hotmail.com>
2025-03-04 10:52:29 -0500

    code playGame func, declare winner

--- script.js
+++ script.js
@@ -15,22 +15,26 @@ function getHumanChoice() {
     return humanChoice
 }
 
+let computerScore = 0
+let humanScore = 0
+
 function playRound(humanChoice, computerChoice) {
-    humanChoice == humanChoice.toLowerCase()
+    (humanChoice == humanChoice.toLowerCase())
      if (humanChoice == computerChoice) {
         console.log('Tie!')
         return 'tie'
      } else if (computerChoice == 'rock') {
-        if (humanChoice == 'scissors') {
+        if (humanChoice == 'paper') {
+            console.log('you win! paper beats rock');
+            ++humanScore
+            return 'win'
+        } else if (humanChoice == 'scissors') {
             console.log('you lose! rock beats scissors!');
             ++computerScore
             return 'loss'
-        } else if (humanChoice == 'paper') {
-            console.log('you win! paper beats rock!');
-            ++humanScore
-            return 'win'
         } else {
             console.log('invalid choice');
+            return 'invalid'
         } 
     } 
     else if (computerChoice = 'paper') {
@@ -44,6 +48,7 @@ function playRound(humanChoice, computerChoice) {
                 return 'win'
             } else {
                 console.log('invalid choice');
+                return 'invalid'
             }
         } 
         else if (computerChoice == 'scissors') {
@@ -57,19 +62,30 @@ function playRound(humanChoice, computerChoice) {
                 return 'loss'
             } else {
                 console.log('invalid choice');
+                return 'invalid'
             }
         }    
 }
 
 
 function playGame() {
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
     for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        console.log('--------------')
         playRound(humanSelection, computerSelection)
-        console.log(humanSelection)
-        console.log(computerSelection)
-         
+        console.log('Human Choice', humanSelection)
+        console.log('Computer Choice', computerSelection) 
+        console.log('Human Score', humanScore) 
+        console.log('Computer Score', computerScore)
+} 
+if (humanScore > computerScore) {
+    console.log('you win!')
+} else if (computerScore > humanScore) {
+    console.log('you lose! try again!')
+} else {
+    console.log('tie!')
 }
+
 }
 playGame()
